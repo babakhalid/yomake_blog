064937f06166ba493d055fbef314c366e2ba9f8c
Nia <nia@yomake.ai>
2026-04-20 09:07:53 +0000

    add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import { Layout } from '@/components';
 import { HomePage, ArticlesPage, ArticlePage } from '@/pages';
 import './index.css';
@@ -13,6 +14,7 @@ function App() {
           <Route path="articles/:slug" element={<ArticlePage />} />
         </Route>
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 }
